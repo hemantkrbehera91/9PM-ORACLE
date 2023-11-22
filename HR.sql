df186562120af5ef7bf08f0f6fb856c241de46fc
hemantkrbehera <hemant.krbehera91@gmail.com>
2023-11-22 21:23:41 +0530

    THIS IS A TOPIC ABOUT VERSIONING TOOL

---
+++ HR.sql
@@ -0,0 +1,25 @@
+
+dev server---->sit server --->uat server--->production
+
+
+versioning tool--- GIT
+
+
+CLIENT REQUIREMENT:(CHANGE REQUEST)
+      CHANGES   -- 1ST VERSION
+       
+       SOME CHANGES  -- 2ND VERSION
+       
+       SOME MORE CHANGES   --3RD VERSION
+       
+       
+1>CLONE
+2>ADD
+3>COMMIT
+4>PUSH
+
+
+LOCAL SERVER--->STAGING AREA--->GLOBAL SERVER
+
+
+SOURCE TREE
